2b3e93ed5748c1174631575726c0c5214c77b737
Elise Spain <elisespain@gmail.com>
2019-08-09 09:57:26 -0700

    Created array of people

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+let people = [
+	{
+		id: 0,
+		name: 'Maverick Jones'
+	},
+
+	{
+		id: 1,
+		name: 'Aaron Jones'
+	},
+	{
+		id: 2,
+		name: 'Tas Mcgowen'
+	}
+];
+
 server.listen(8000, () => console.log('API is running on port 8000'));
