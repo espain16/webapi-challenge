46ea04b1420dd0551e15e039276109b50bb889e3
Elise Spain <elisespain@gmail.com>
2019-08-09 10:25:57 -0700

    Adjusted my data source

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-let people = [
+let data = [
 	{
 		id: 0,
-		name: 'Maverick Jones'
-	},
-
-	{
-		id: 1,
-		name: 'Aaron Jones'
-	},
-	{
-		id: 2,
-		name: 'Tas Mcgowen'
+		name: 'Mike Fierce',
+		chores: [
+			{
+				id: 1,
+				description: 'wash the dishes and vaccum the floor',
+				notes: 'make sure you wash the dishes thoroughly and vaccum every room',
+				assignedTo: 1,
+				completed: true
+			},
+			{
+				id: 2,
+				description: 'power wash the house',
+				notes: 'dont spray anyone or anything other then the house and avoid the windows',
+				assignedTo: 1,
+				completed: false
+			}
+		]
 	}
 ];
-
 server.listen(8000, () => console.log('API is running on port 8000'));
