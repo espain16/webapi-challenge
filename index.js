703652b47b96792ec07df9c11ee6b9ff7345b21f
Elise Spain <elisespain@gmail.com>
2019-08-09 13:43:56 -0700

    Redo of the hard coded array

--- index.js
+++ index.js
@@ -3,26 +3,13 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-let data = [
+const people = [
 	{
 		id: 0,
-		name: 'Mike Fierce',
-		chores: [
-			{
-				id: 1,
-				description: 'wash the dishes and vaccum the floor',
-				notes: 'make sure you wash the dishes thoroughly and vaccum every room',
-				assignedTo: 1,
-				completed: true
-			},
-			{
-				id: 2,
-				description: 'power wash the house',
-				notes: 'dont spray anyone or anything other then the house and avoid the windows',
-				assignedTo: 1,
-				completed: false
-			}
-		]
+		name: 'Harry Potter'
+	},
+	{
+		id: 1,
+		name: 'Luna Lovegood'
 	}
 ];
-server.listen(8000, () => console.log('API is running on port 8000'));
